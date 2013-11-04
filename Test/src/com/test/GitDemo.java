59e220e1b982c05e1d7c1ac7f89c1e22465b212b
gaoyanzhen <g_y_zhen@163.com>
2013-11-04 10:09:00 +0800

    first commit

---
+++ Test/src/com/test/GitDemo.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class GitDemo {
+
+	/**
+	 * @param args
+	 * @see 
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git!");
+		System.out.println("测试");
+	}
+
+}
