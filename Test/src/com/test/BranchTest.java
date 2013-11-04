98dd7a42cc34ece881aed5c0bdf19a8728c3416e
gaoyanzhen <g_y_zhen@163.com>
2013-11-04 15:38:51 +0800

    0

--- Test/src/com/test/BranchTest.java
+++ Test/src/com/test/BranchTest.java
@@ -12,6 +12,8 @@ public class BranchTest {
 		System.out.print("test check out 456!");
 		System.out.print("test check out 789!");
 		System.out.print("test check out 0!");
+		
+		System.out.print("0!");
 
 	}
 
