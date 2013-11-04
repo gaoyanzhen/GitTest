c4c42de7acd61679cd07aeab8cc7ef4260188391
gaoyanzhen <g_y_zhen@163.com>
2013-11-04 15:16:40 +0800

    test 456

--- Test/src/com/test/BranchTest.java
+++ Test/src/com/test/BranchTest.java
@@ -9,6 +9,7 @@ public class BranchTest {
 	public static void main(String[] args) {
 		System.out.println("BranchTest!");
 		System.out.print("test check out!");
+		System.out.print("test check out 456!");
 
 	}
 
